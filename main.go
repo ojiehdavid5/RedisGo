70482b70497996a6309767351f8ac431966a30bb
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-05-01 10:41:45 +0100

    working with redis

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"net/http"
+	"github.com/redis/go-redis/v9"
+	"context"
+	"log"
+	"time"
+
+)
 func main() {
-fmt.Println("Hello, World!")
+
+	
+
+
+
+
+
+
+
+	
+// fmt.Println("Building Rest API using Go and REDIS")
+// mux:= http.NewServeMux();
+// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// fmt.Fprint(w, "Hello, World!")
+// })
+
+// if err:= http.ListenAndServe(":8080", mux); err != nil {
+// 	fmt.Print(err.Error())
+// }
 }
